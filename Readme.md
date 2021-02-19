b2c1d6b1450b4aa53283081fe61441835f34a6e5
Francisco López <flopez_1384@hotmail.com>
2021-02-19 12:15:51 -0600

    [ADD] The routes, router and index files of the project

---
+++ Readme.md
@@ -0,0 +1,4 @@
+#Start de project
+##To exceuto the project please proced with the following command:
+
+### http-server-spa . ./index.html
---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+  {
+    path: "/",
+    template: "<h1>Hello again my friend HOME</h1>",
+  },
+  {
+    path: "/contact",
+    template: "<h1>Hello my friend CONTACT</h1>",
+  },
+  {
+    path: "/aboutme",
+    template: "<h1>Hello my friend ABOUT ME</h1>",
+  },
+];
